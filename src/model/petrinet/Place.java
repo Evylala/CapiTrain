d8ce83ba8dbc5a560da9f209012c6dc2d6ce3661
Evylala <evylala@gmail.com>
2015-10-29 09:54:30 +0100

    modif petrinets

--- src/model/petrinet/Place.java
+++ src/model/petrinet/Place.java
@@ -4,4 +4,35 @@ import java.util.ArrayList;
 
 public class Place extends NamedElement {
 	private ArrayList<Transition> postt;
+	private ArrayList<Transition> pret;
+	private Net net;
+	
+	public Place(String name, String id) {
+		super(name, id);
+	}
+
+	public ArrayList<Transition> getPostt() {
+		return postt;
+	}
+
+	public void setPostt(ArrayList<Transition> postt) {
+		this.postt = postt;
+	}
+
+	public ArrayList<Transition> getPret() {
+		return pret;
+	}
+
+	public void setPret(ArrayList<Transition> pret) {
+		this.pret = pret;
+	}
+
+	public Net getNet() {
+		return net;
+	}
+
+	public void setNet(Net net) {
+		this.net = net;
+	}
+	
 }
